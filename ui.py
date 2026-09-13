7f518ad9d3d31dcaba55873546ec2667830e2829
Gavin Hipszer <ghipszer@terpmail.umd.edu>
2026-09-13 00:00:51 -0400

    Update and rename UI.py to ui.py

--- ui.py
+++ ui.py
@@ -1,12 +1,13 @@
 from game import Game
+import data as dta
 
 
 def valid_ending_square(square: str) -> bool:
     if len(square) != 2 or not square[0].isalpha() or not square[1].isdigit():
         return False
 
-    valid_letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' }
-    valid_numbers = { 1, 2, 3, 4, 5, 6, 7, 8 }
+    valid_letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}
+    valid_numbers = {1, 2, 3, 4, 5, 6, 7, 8}
 
     if square[0].lower() not in valid_letters or (int(square[1])) not in valid_numbers:
         return False
@@ -50,7 +51,7 @@ def main():
                 print("Invalid starting square, enter again")
 
             start_x, start_y = starting_square[0], starting_square[1]
-            start_x, start_y = 8 - int(start_y), game.col_map[start_x]
+            start_x, start_y = 8 - int(start_y), dta.col_map[start_x]
 
             while True:
                 ending_square = input("Enter desired square to move it to (ex: a2): ")
@@ -64,7 +65,7 @@ def main():
                         continue
 
                 end_x, end_y = ending_square[0], ending_square[1]
-                end_x, end_y = 8 - int(end_y), game.col_map[end_x]
+                end_x, end_y = 8 - int(end_y), dta.col_map[end_x]
 
                 if game.valid_move(start_x, start_y, end_x, end_y):
                     if game.landing_on_enemy_pieces(end_x, end_y):
@@ -80,13 +81,21 @@ def main():
                     continue
 
         temp = game.get_square(start_x, start_y)
-        game.set_square("  ", start_x, start_y)
+        game.set_square(dta.EMPTY, start_x, start_y)
         game.set_square(temp, end_x, end_y)
 
-        if temp[1] == "P" and end_x == 0 or end_x == 7:
+        if temp.get_name()[1] == "P" and end_x == 0 and game.get_curr_player() == "White":
             while True:
                 promote_to = input("Promote your pawn to a knight, bishop, rook, or queen  (ex: wN): ")
-                if promote_to in game.get_white_pieces_map() or promote_to in game.get_black_pieces_map():
+                if game.valid_promotion_piece(promote_to):
+                    game.promote_pawn(promote_to, end_x, end_y)
+                    break
+                else:
+                    print("Invalid argument")
+        elif temp.get_name()[1] == "P" and end_x == 7 and game.get_curr_player() == "Black":
+            while True:
+                promote_to = input("Promote your pawn to a knight, bishop, rook, or queen  (ex: wN): ")
+                if game.valid_promotion_piece(promote_to):
                     game.promote_pawn(promote_to, end_x, end_y)
                     break
                 else:
@@ -94,5 +103,7 @@ def main():
 
         game.change_curr_player()
 
+
 if __name__ == "__main__":
     main()
+
